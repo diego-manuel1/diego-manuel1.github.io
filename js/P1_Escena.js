4e3b4e155c8747193769b73bfcacabbc7d49d1f0
diego-manuel1 <99796304+diego-manuel1@users.noreply.github.com>
2024-02-18 16:32:24 +0100

    P1 -> Haciendo más grande el modelo

--- js/P1_Escena.js
+++ js/P1_Escena.js
@@ -147,9 +147,9 @@ function loadScene()
             gltf.scene.position.y = 0;
             gltf.scene.rotation.y = -Math.PI/2;
             pentObject.add( gltf.scene );
-            gltf.scene.rotation.x = gltf.scene.rotation.x * 2;
-            gltf.scene.rotation.y = gltf.scene.rotation.y * 2;
-            gltf.scene.rotation.z = gltf.scene.rotation.z * 2;
+            gltf.scene.scale.x = gltf.scene.scale.y * 2;
+            gltf.scene.scale.y = gltf.scene.scale.y * 2;
+            gltf.scene.scale.z = gltf.scene.scale.z * 2;
             console.log("LADY OFFICER");
             console.log(gltf);
         
