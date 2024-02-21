82fbcca5fe4848a2f96711cee91780234de9bb59
diego-manuel1 <99796304+diego-manuel1@users.noreply.github.com>
2024-02-21 17:04:56 +0100

    P2 -> Preparando P2

--- js/P2_EscenaAnimada.js
+++ js/P2_EscenaAnimada.js
@@ -13,7 +13,11 @@
 /*******************
  * TO DO: Cargar los modulos necesarios
  *******************/
-
+import * as THREE from "../lib/three.module.js";
+import {GLTFLoader} from "../lib/GLTFLoader.module.js";
+import {OrbitControls} from "../lib/OrbitControls.module.js";
+import {TWEEN} from "../lib/tween.module.min.js";
+import {GUI} from "../lib/lil-gui.module.min.js";
 // Variables de consenso
 let renderer, scene, camera;
 
@@ -36,27 +40,106 @@ function init()
     /*******************
     * TO DO: Completar el motor de render y el canvas
     *******************/
+    document.getElementById('container').appendChild( renderer.domElement );
 
     // Escena
     scene = new THREE.Scene();
-    
+    scene.background = new THREE.Color(0.5,0.5,0.5);
     // Camara
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1,1000);
     camera.position.set( 0.5, 2, 7 );
     /*******************
     * TO DO: Añadir manejador de camara (OrbitControls)
     *******************/
+    cameraControls = new OrbitControls( camera, renderer.domElement );
+    cameraControls.target.set(0,1,0);
     camera.lookAt( new THREE.Vector3(0,1,0) );
 }
 
 function loadScene()
 {
-    const material = new THREE.MeshNormalMaterial( {wireframe:false} );
-
+    //const material = new THREE.MeshNormalMaterial( {wireframe:false} );
+    //Cambiamos de momento a un basic material
+    const material = new THREE.MeshBasicMaterial( { color: 'yellow', wireframe: true } );
     /*******************
     * TO DO: Misma escena que en la practica anterior
     *******************/
+    const suelo = new THREE.Mesh( new THREE.PlaneGeometry(10,10, 10,10), material );
+    suelo.rotation.x = -Math.PI / 2;
+    scene.add(suelo);
+    // Creamos el pentagono para posicionar las figuras
+    //const geoPent = new THREE.CylinderGeometry( 1, 5, 1, 5, 1);
+    //Creamos la geometría de las figuras
+    const geoCubo = new THREE.BoxGeometry( 2,2,2 );
+    const geoEsfera = new THREE.SphereGeometry( 1, 20,20 );
+    const geoCone = new THREE.ConeGeometry( 1, 5, 8, 1);
+    const geoCylinder = new THREE.CylinderGeometry( 1, 1, 2);
+    const geoCapsule = new THREE.CapsuleGeometry(1, 5, 1);
+    //Creamos la mesh con la geometría y el material
+    
+    const cubo = new THREE.Mesh( geoCubo, material );
+    const esfera = new THREE.Mesh( geoEsfera, material );
+    const cone = new THREE.Mesh( geoCone, material );
+    const cylinder = new THREE.Mesh( geoCylinder, material );
+    const capsule = new THREE.Mesh( geoCapsule, material );
+    figures = [cubo, esfera, cone, cylinder, capsule];
+    //Creamos la forma del pentagono y posicionamos sobre sus vertices a las figuras
+    const pentShape = new THREE.Shape();
+    const pentRadius = 4;
+    const pentSides = 5;
 
+    for (let i = 0; i < pentSides; i++) {
+        let angle = (i / pentSides) * Math.PI * 2;
+        //let angle = (i / pentSides) * (-Math.PI/2);
+        let x = Math.cos(angle) * pentRadius;
+        let y = Math.sin(angle) * pentRadius;
+        if (i === 0) {
+            pentShape.moveTo(x, y);
+        } else {
+            pentShape.lineTo(x, y);
+        }
+        //Colocamos la figura en la posición
+        figures[i].position.x = x;
+        figures[i].position.y = y;
+    }
+    //Creamos la geometría del pentagono
+    const geoPent = new THREE.ShapeGeometry( pentShape );
+    const pent = new THREE.Mesh( geoPent, material );
+    //Hacemos hijos del mesh del pentagono al resto de mesh y los rotamos para que sean paralelos al pentagono
+    for(let i = 0; i < figures.length; i++){
+        pent.add(figures[i]);
+        figures[i].rotation.x = Math.PI / 2;
+    }
+    //Rotamos el pentagono para que sea paralelo al suelo (también se mueven las figuras para que sean paralelas sobre el plano)
+    pent.rotation.x = -Math.PI / 2;
+    
+    //Creamos el objeto 3D que representa el pentagono
+    pentObject = new THREE.Object3D();
+    pentObject.position.x=0;
+    pentObject.position.y=1;
+    pentObject.position.z=0;
+    pentObject.add(pent);
+    pentObject.add( new THREE.AxesHelper(1) );
+    //Agregamos el objeto a la escena
+    scene.add(pentObject);
+    const glloader = new GLTFLoader();
+    glloader.load( 'models/anime_lady_officer/scene.gltf', function ( gltf ) {
+            gltf.scene.position.y = -1;
+            gltf.scene.rotation.y = -Math.PI/2;
+            pentObject.add( gltf.scene );
+            gltf.scene.scale.x = gltf.scene.scale.y * 2;
+            gltf.scene.scale.y = gltf.scene.scale.y * 2;
+            gltf.scene.scale.z = gltf.scene.scale.z * 2;
+            console.log("LADY OFFICER");
+            model = gltf.scene;
+            console.log(gltf);
+        
+        }, undefined, function ( error ) {
+        
+            console.error( error );
+        
+        } );
+    scene.add( new THREE.AxesHelper(3) );
 }
 
 function loadGUI()
