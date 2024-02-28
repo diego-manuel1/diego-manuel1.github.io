d2175c5ba9bb8aabd41031b44eb4447394bceeb6
diego-manuel1 <99796304+diego-manuel1@users.noreply.github.com>
2024-02-28 12:45:35 +0100

    EscenaChess -> Agregado tablero de ajedrez a la escena (se ve a oscuras)

--- js/EscenaChess.js
+++ js/EscenaChess.js
@@ -7,8 +7,9 @@ import {GUI} from "../lib/lil-gui.module.min.js";
 
 // Variables estandar
 let renderer, scene, camera;
-
+let cameraControls, effectController;
 // Otras globales
+let tableObject;
 
 // Acciones
 init();
@@ -28,29 +29,41 @@ function init()
     scene.background = new THREE.Color(0.5,0.5,0.5);
 
     // Instanciar la camara
-    camera= new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,100);
-    camera.position.set(0.5,2,7);
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1,100);
+    camera.position.set( 0.5, 2, 7 );
+    //Controles de camara
     cameraControls = new OrbitControls( camera, renderer.domElement );
     cameraControls.target.set(0,1,0);
-    camera.lookAt(0,1,0);
-
+    camera.lookAt( new THREE.Vector3(0,1,0) );
     // Eventos
-    renderer.domElement.addEventListener('dblclick', animate );
+    //renderer.domElement.addEventListener('dblclick', animate );
 }
 
 function loadScene()
 {
+    //Creamos el objeto del tablero de ajedrez.
+    tableObject = new THREE.Object3D();
+    loadChessTable();
+    tableObject.position.x = 0;
+    tableObject.position.y = 0;
+    tableObject.position.z = 0;
+    //Agregamos el objeto tablero a la escena.
+    scene.add(tableObject);
+    //Añadimos ejes a la escena.
+    scene.add( new THREE.AxesHelper(3) );
 }
 
-function loadTable(){
+function loadChessTable(){
     // Importar un modelo en gltf
    const glloader = new GLTFLoader();
 
-   glloader.load( 'models/robota/scene.gltf', function ( gltf ) {
-       gltf.scene.position.y = 1;
-       gltf.scene.rotation.y = -Math.PI/2;
-       gltf.scene.name = 'robota';
-       esfera.add( gltf.scene );
+   glloader.load( 'models/chessTable/scene.gltf', function ( gltf ) {
+       gltf.scene.position.y = 0;
+       gltf.scene.position.x = 0;
+       gltf.scene.position.z = 0;
+       gltf.scene.name = 'chessTable';
+       //Agregamos el modelo como hijo del objeto tablero.
+       tableObject.add( gltf.scene );
    
    }, undefined, function ( error ) {
    
